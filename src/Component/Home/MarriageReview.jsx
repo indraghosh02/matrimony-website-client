41b8ba019eafe6948f56f988674e73a9ffb0c9f8
indraghosh02 <indraghosh0802@gmail.com>
2024-06-02 17:32:40 +0600

    sorted reviews

--- src/Component/Home/MarriageReview.jsx
+++ src/Component/Home/MarriageReview.jsx
@@ -1,9 +1,4 @@
-
-
 import { useQuery } from "@tanstack/react-query";
-
-
-
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 import ReviewCard from "./ReviewCard";
 
@@ -13,17 +8,16 @@ const MarriageReview = () => {
         queryKey: ['review'],
         queryFn: async () => {
             const { data } = await axiosSecure.get('/marriage');
-            return data;
+            // Sort the data array in descending order based on the date
+            return data.sort((a, b) => new Date(b.date) - new Date(a.date));
         }
     });
 
     return (
-        
-       <div className='mt-12' >
+       <div className='mt-16'>
          <h2 className='text-4xl font-bold mb-4 text-center text-red-600 font-serif'>Our Success Stories</h2>
         <div className='flex p-10'>
-           
-            <div className='grid lg:grid-cols-3 '>
+            <div className='grid lg:grid-cols-3 gap-4'>
                 {data && data.map(review => (
                     <ReviewCard key={review._id} review={review} />
                 ))}
@@ -34,4 +28,3 @@ const MarriageReview = () => {
 };
 
 export default MarriageReview;
-
