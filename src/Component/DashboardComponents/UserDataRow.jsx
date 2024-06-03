a29cf3753001f820c233d7808125f00da3fb9d6e
indraghosh02 <indraghosh0802@gmail.com>
2024-06-03 17:43:03 +0600

    made admin

--- src/Component/DashboardComponents/UserDataRow.jsx
+++ src/Component/DashboardComponents/UserDataRow.jsx
@@ -1,8 +1,47 @@
 import PropTypes from 'prop-types'
+import { useContext, useState } from 'react'
+import UpdateUserModal from './UpdateUserModal'
+import { useMutation } from '@tanstack/react-query'
+import useAxiosPublic from '../../hooks/useAxiosPublic'
+import { AuthContext } from '../../providers/AuthProvider'
+import { toast } from 'react-toastify'
+
 const UserDataRow = ({ user, refetch }) => {
+  const { user: loggedInUser } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false)
+  const axiosPublic = useAxiosPublic()
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (updatedUser) => {
+      const { data } = await axiosPublic.patch(`/users/update/${user.email}`, updatedUser)
+      return data
+    },
+    onSuccess: () => {
+      refetch()
+      toast.success('User role updated successfully')
+      setIsOpen(false)
+    },
+    onError: (error) => {
+      console.error(error)
+      toast.error('Failed to update user role')
+    }
+  })
+
+  // Modal handler
+  const modalHandler = async () => {
+    const updatedUser = { role: "admin" }
+
+    try {
+      await mutateAsync(updatedUser)
+    } catch (err) {
+      console.error(err)
+      toast.error('Failed to update user role')
+    }
+  }
+
   return (
     <tr>
-        <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>{user?.name}</p>
       </td>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
@@ -11,40 +50,17 @@ const UserDataRow = ({ user, refetch }) => {
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
         <p className='text-gray-900 whitespace-no-wrap'>{user?.role}</p>
       </td>
-  
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <button className='btn bg-green-600 text-white  px-4 py-2 rounded hover:bg-blue-600'> Make Admin </button>
-       
+        <button onClick={modalHandler} className='btn bg-green-600 text-white px-4 py-2 rounded hover:bg-blue-600'>
+          Make Admin
+        </button>
       </td>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <button className='btn bg-yellow-400 text-white  px-4 py-2 rounded hover:bg-blue-600'> Make Premium </button>
-       
+        <button  className='btn bg-yellow-400 text-black px-4 py-2 rounded hover:bg-blue-600'>
+          Make premium
+        </button>
       </td>
-      {/* <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        {user?.status ? (
-          <p
-            className={`${
-              user.status === 'Verified' ? 'text-green-500' : 'text-yellow-500'
-            } whitespace-no-wrap`}
-          >
-            {user.status}
-          </p>
-        ) : (
-          <p className='text-red-500 whitespace-no-wrap'>Unavailable</p>
-        )}
-      </td> */}
-
-      {/* <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
-          <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
-          ></span>
-          <span className='relative'>Update Role</span>
-        </span>
-       
-      </td> */}
-      
+     
     </tr>
   )
 }
